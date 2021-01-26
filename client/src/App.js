78be1ad6814da25af7c828397608ce9b38e93b47
cyrus8050 <ckmobile8050@gmail.com>
2021-01-26 15:48:05 +0800

    init

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  fetch('http://localhost:5000/greeting').then(res =>
+    res.json()
+  ).then(data => console.log(data))
+  return (
+    <div className="App">
+      app
+    </div>
+  );
+}
+
+export default App;
